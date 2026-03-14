cd99eb0bbfb479b9c857bffd799b712f38410be0
unknown <ricvera@favoritafc.com>
2026-03-14 17:21:28 -0500

    Add CORS to grant frontend

--- src/main/java/com/example/orders/OrderController.java
+++ src/main/java/com/example/orders/OrderController.java
@@ -1,5 +1,6 @@
 package com.example.orders;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
